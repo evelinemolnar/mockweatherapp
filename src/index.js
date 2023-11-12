ab2b6f7077ff30424bd8de644d8acb287f3023c3
evelinemolnar <noemi.eveline.molnar@stud.ubbcluj.ro>
2023-11-12 17:39:02 +0200

    injected icon, details, change of style

--- src/index.js
+++ src/index.js
@@ -1,64 +1,65 @@
-function search(event) {
-    event.preventDefault();
-    let searchInputElement = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#current-city");
-    cityElement.innerHTML = searchInputElement.value;
-    // Add a call to your new function when the search button is clicked
-    handleSearch(searchInputElement.value);
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
-  
-  function formatDate(date) {
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let day = date.getDay();
-  
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-  
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-  
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-  
-    let formattedDay = days[day];
-    return `${formattedDay} ${hours}:${minutes}`;
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+  function refreshWeather(response){
+    let tempElement = document.querySelector("#current-temperature-value");
+    let descriptionElement = document.querySelector("#current-details-description");
+    let humidityElement = document.querySelector("#current-details-humidity");
+    let windElement = document.querySelector("#current-details-wind");
+    let cityElement = document.querySelector("#current-city");
+    let timeElement = document.querySelector("#current-date");
+    let date = new Date(response.data.dt * 1000);
+    let iconElement = document.querySelector("#icon");
+
+    console.log("Current Temp:", response.data);
+    cityElement.innerHTML = response.data.name;
+    timeElement.innerHTML = formatDate(date);
+
+    tempElement.innerHTML = Math.round(response.data.main.temp);
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = response.data.wind.speed;
+    iconElement.innerHTML = `<img src="http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png" alt="weather icon" />`;
   }
   
   function handleSearch(city) {
     const apiKey = "11a4762111955564b01f416b85389b2f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     console.log(`Search for city: ${city}`);
   
-    // Add your logic for handling the search here
     axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-      )
-      .then((response) => {
-        console.log("Current Temp:", response.data);
-        const temperature = response.data.main.temp;
-        let tempElement = document.querySelector("#current-temperature-value");
-        tempElement.innerHTML = temperature;
-      })
+      .get(apiUrl)
+      .then(refreshWeather)
       .catch((error) => {
         console.error("Error fetching weather:", error);
       });
   }
   
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#search-input");
+    handleSearch(cityInputElement.value);
+  }
+
   let searchForm = document.querySelector("#search-form");
-  searchForm.addEventListener("submit", search);
-  
-  let currentDateElement = document.querySelector("#current-date");
-  let currentDate = new Date();
-  
-  currentDateElement.innerHTML = formatDate(currentDate);
-  
+  searchForm.addEventListener("submit", handleSearchSubmit);
+
+ handleSearch("Paris");
